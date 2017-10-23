6c6de438dde515ae804ade692db234eca770992c
moritz <moritz.fickenscher@fau.de>
2017-10-23 18:23:09 +0000

    IGU-00 check if db is migrated before fix site object

--- src/issue/apps.py
+++ src/issue/apps.py
@@ -10,6 +10,7 @@ work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
 """
 from django.apps import AppConfig
 from django.conf import settings
+from django.db import connection
 
 
 class IssueConfig(AppConfig):
@@ -18,7 +19,12 @@ class IssueConfig(AppConfig):
     def ready(self):
         import issue.signals
         from django.contrib.sites.models import Site
-        site = Site.objects.get_current()
-        site.domain = settings.HOST
-        site.name = settings.HOST 
-        site.save()
+
+        def db_table_exists(table_name):
+            return table_name in connection.introspection.table_names()
+
+        if db_table_exists('django_site'):
+            site = Site.objects.get_current()
+            site.domain = settings.HOST
+            site.name = settings.HOST
+            site.save()
