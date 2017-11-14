757aa7f6b1d7191a22f23fa5705c28e56d7568bd
cauca <christian.strate@fau.de>
2017-11-14 23:32:00 +0100

    decreased the password requirements a bit - now they are allowed to be more similar to email, username, first and last name than before

--- src/common/settings/common.py
+++ src/common/settings/common.py
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
         'OPTIONS': {
             'user_attributes': ['username', 'first_name', 'last_name', 'email'],
-            'max_similarity': 0.4,
+            'max_similarity': 0.5,
         }
     },
     {
