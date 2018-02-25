be7a14dfb011723e549117bb64b1ee8dee2f53ce
cauca <christian.strate@fau.de>
2018-02-25 21:58:10 +0100

    IGU-227 replaced deprecated filter parameter "name" with "field_name"

--- src/api/filter.py
+++ src/api/filter.py
@@ -37,15 +37,15 @@ class UserFilter(filters.FilterSet):
 
 
 class IssueFilter(filters.FilterSet):
-    status = filters.CharFilter(name='kanbancol__name', lookup_expr='exact')
-    assignee = filters.CharFilter(name="assignee__username", lookup_expr='contains')
-    participant = filters.CharFilter(name="participant__username", lookup_expr='contains')
-    dependsOn = filters.NumberFilter(name="dependsOn__number", lookup_expr='exact')
-    sprint = filters.NumberFilter(name="sprint__seqnum", lookup_expr='exact')
-    project = filters.CharFilter(name="project__name_short", lookup_expr='exact')
-    logged_total__gt = DurationFilter(name='logged_total', lookup_expr='gt')
-    logged_total__lt = DurationFilter(name='logged_total', lookup_expr='lt')
-    logged_total = DurationFilter(name='logged_total', lookup_expr='exact')
+    status = filters.CharFilter(field_name='kanbancol__name', lookup_expr='exact')
+    assignee = filters.CharFilter(field_name="assignee__username", lookup_expr='contains')
+    participant = filters.CharFilter(field_name="participant__username", lookup_expr='contains')
+    dependsOn = filters.NumberFilter(field_name="dependsOn__number", lookup_expr='exact')
+    sprint = filters.NumberFilter(field_name="sprint__seqnum", lookup_expr='exact')
+    project = filters.CharFilter(field_name="project__name_short", lookup_expr='exact')
+    logged_total__gt = DurationFilter(field_name='logged_total', lookup_expr='gt')
+    logged_total__lt = DurationFilter(field_name='logged_total', lookup_expr='lt')
+    logged_total = DurationFilter(field_name='logged_total', lookup_expr='exact')
 
     class Meta:
         model = Issue
@@ -75,9 +75,9 @@ class CommentFilter(filters.FilterSet):
 
 
 class TimelogFilter(filters.FilterSet):
-    time__gt = DurationFilter(name='time', lookup_expr='gt')
-    time__lt = DurationFilter(name='time', lookup_expr='lt')
-    time = DurationFilter(name='time', lookup_expr='exact')
+    time__gt = DurationFilter(field_name='time', lookup_expr='gt')
+    time__lt = DurationFilter(field_name='time', lookup_expr='lt')
+    time = DurationFilter(field_name='time', lookup_expr='exact')
 
     class Meta:
         model = Timelog
