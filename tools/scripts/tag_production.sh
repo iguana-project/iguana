9bee484b35f0f1d189958971899a6e98997b8c12
cauca <christian.strate@fau.de>
2018-03-14 05:39:57 +0100

    IGU-422 added comment to tag_production script

--- tools/scripts/tag_production.sh
+++ tools/scripts/tag_production.sh
@@ -10,6 +10,9 @@ You should have received a copy of the license along with this
 work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
 '
 
+# this script creates a new production-tag to notify the server about a new deployable version
+# don't forget to push the created tag with "git push --tags"
+
 num=$(git tag --sort=version:refname -l production-* | tail -n 1 | cut -d- -f2)
 num=$(($num + 1))
 git tag production-$num
