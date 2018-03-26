d5347bf6ec29bdcc5888ea1a126547a2521e8fe0
cauca <christian.strate@fau.de>
2018-03-27 01:47:01 +0200

    fixed some typos

--- src/search/fieldcheckings.py
+++ src/search/fieldcheckings.py
@@ -11,12 +11,12 @@ work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
 
 
 # This is pretty much just a logical hint for classes to be searchable.
-# In most cases these functionalities are overwritten in the classes mixed this Mixin in
+# In most cases these functionalities are overwritten in the classes mixed this mixin in
 class SearchableMixin():
-    # NOTE: This shall be overwritten in the classes that mix this Mixin in, otherwise this mixin is not usefull there
+    # NOTE: This shall be overwritten in the classes that mix this mixin in, otherwise this mixin is not useful there
     searchable_fields = []
 
-    # This shall be overwritten in most of the classes that mix this Mixin in
+    # This shall be overwritten in most of the classes that mix this mixin in
     def search_allowed_for_user(self, user):
         return True
 
