d09528e49f2ffe4913f6907de737db8c662fcfd5
cauca <christian.strate@fau.de>
2018-08-07 19:31:56 +0200

    updated the SECRET_KEY generator

--- tools/scripts/generateSecretDjangoKey.py
+++ tools/scripts/generateSecretDjangoKey.py
@@ -1,9 +1,11 @@
 #!/bin/python
 
 import random
+import string
 
 # generate secret key
-SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
-                      for _ in range(50)])
+SECRET_KEY = ''.join([random.SystemRandom().choice(string.digits + string.ascii_uppercase
+                      + string.ascii_lowercase + '!@#$%^&*(-_=+)')
+                     for _ in range(50)])
 
 print(SECRET_KEY, end="")
