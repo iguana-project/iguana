0dc559006c27673c4f34c79fd128ab12e3a4ebc5
mammo0 <marc.ammon@hotmail.de>
2019-08-22 07:04:20 +0200

    IGU-00 always start the cron daemon if not in development mode

--- docker/docker_entrypoint.py
+++ docker/docker_entrypoint.py
@@ -59,10 +59,12 @@ for dir in [BASE_DIR, FILES_DIR]:
         # don't forget the root directory
         chown(dir, IGUANA_PUID, IGUANA_PGID, follow_symlinks=False)
 
-# start nginx and cron daemon (for logrotate) as root
-if VARIANT != "development" and USE_NGINX:
-    # start nginx
-    Popen(["nginx", "-c", path.join(FILES_DIR, "nginx.conf")])
+# start helper processes as root
+if VARIANT != "development":
+    if USE_NGINX:
+        # start nginx if it is used
+        Popen(["nginx", "-c", path.join(FILES_DIR, "nginx.conf")])
+
     # start cron for logrotate
     Popen(["crond", "-L", path.join(FILES_DIR, "logs", "cron.log")])
 
