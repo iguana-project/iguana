fefa0cc019cf44fe0d610a36084d5294b7dbef7a
mammo0 <marc.ammon@hotmail.de>
2019-12-08 17:20:46 +0100

    IGU-460 fixed login/-out and password reset views
    -> these views must be replaced because of deprecation of previous
    methods

--- src/user_management/views.py
+++ src/user_management/views.py
@@ -11,7 +11,8 @@ work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
 from django.views.generic import View
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import password_reset, password_reset_done
+from django.contrib.auth.views import LoginView as D_LIV, LogoutView as D_LOV, \
+                                      PasswordResetView as D_PRV, PasswordResetDoneView as D_PRDV
 from django.contrib import messages
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
@@ -32,64 +33,31 @@ from .generate_activation_token import account_activation_token
 # NOTE: ugettext_lazy "is essential when calls to these functions are located in code
 #       paths that are executed at module load time."
 from django.utils.translation import ugettext as _, ugettext_lazy as _l
+from django.urls.base import reverse_lazy
 
 
 # TODO FormView might help to simplify this class
-class LoginView(View):
-    form_class = AuthenticationForm
+class LoginView(D_LIV):
     hide_breadcrumbs = True
+    template_name = "registration/login.html"
 
-    def get_context_data(self, **kwargs):
-        context = super(LoginView, self).get_context_data(**kwargs)
-        context['next'] = self.request.REQUEST.get('next')
-        return context
-
-    def post(self, request):
-        form = self.form_class(data=request.POST)
-
-        # NOTE: authenticate is already called internally in form.clean
-        if form.is_valid():
-            username_or_email = request.POST['username']
-            password = request.POST['password']
-            # NOTE: authenticate is already called internally in form.clean so there is no need to verify the result
-            user = authenticate(username=username_or_email, password=password)
-            login(request, user)
-
-            request.session[translation.LANGUAGE_SESSION_KEY] = user.language
-            redirect_to = request.POST.get('next', request.GET.get('next', reverse('landing_page:home')))
-            redirect_to = (redirect_to
-                           if is_safe_url(redirect_to, request.get_host())
-                           else reverse('landing_page:home'))
-            if redirect_to == "":
-                return HttpResponseRedirect(reverse("landing_page:home"))
-            else:
-                return HttpResponseRedirect(redirect_to)
-
-        return render(request, 'registration/login.html', {'form': form})
-
-    def get(self, request):
-        # user that are logged in and don't want to come to this page through the LoginRequiredMixin
-        # get redirected to their home
-        if request.user.is_active and request.GET.get('next', '') == '':
-            return render(request, 'landing_page/home.html')
-
-        redirect_to = request.POST.get('next', request.GET.get('next', ''))
-        redirect_to = (redirect_to
-                       if is_safe_url(redirect_to, request.get_host())
-                       else '')
-        return render(request, 'registration/login.html', {'form': self.form_class(), 'next': redirect_to})
+    def get_success_url(self):
+        url = self.get_redirect_url()
+        return url or reverse("landing_page:home")
 
 
-class LogoutView(View):
-    form_class = AuthenticationForm
+class LogoutView(D_LOV):
     hide_breadcrumbs = True
+    template_name = "registration/login.html"
+    next_page = reverse_lazy("login")
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         logged_out = 0
         if request.user.is_active:
             logout(request)
             logged_out = 1
-        return render(request, 'registration/login.html', {'form': self.form_class(), 'logged_out': logged_out})
+        self.extra_context = {'form': AuthenticationForm(), 'logged_out': logged_out}
+        return D_LOV.get(self, request, *args, **kwargs)
 
 
 # https://docs.djangoproject.com/en/1.10/topics/forms/
@@ -142,7 +110,7 @@ class SignUpView(View):
                     'platform': settings.PLATFORM,
                     # url-elements
                     'domain': get_current_site(request).domain,
-                    'uid': urlsafe_base64_encode(force_bytes(new_user.pk)).decode(),
+                    'uid': urlsafe_base64_encode(force_bytes(new_user.pk)),
                     'token': account_activation_token.make_token(new_user),
                 }),
                 # TODO as soon as there is a parameter in the settings to provide the from field it shall be used here
@@ -197,32 +165,12 @@ class VerifyEmailAddress(View):
 # TODO reactivate as soon as the bug is fixed
 # TODO BUG does it work as intended again?
 # TODO BUG sends wrong url for local (development) settings example.com != localhost:port
-class PasswordResetView(View):
+class PasswordResetView(D_PRV):
     breadcrumb = _l("Reset password")
-
-    def get(self, request):
-        from django.conf import settings
-        return password_reset(request, 'registration/password_reset_form.html')
-
-    def post(self, request):
-        email = request.POST['email']
-        # NOTE this does not provide the email address yet, since it seems like there
-        #      is a bug in django (at least imo). The path with HttpResponseRedirect omits the extra_context-field
-        # s.a. http://python.6.x6.nabble.com/Django-24944-Have-password-reset-pass-extra-
-        #        context-to-the-email-template-rendering-as-well-td5097076.html
-        # TODO did I mean url instead of email address?
-        return password_reset(request, template_name='registration/password_reset_form.html',
-                              post_reset_redirect='password_reset_done', extra_context={'email': email})
+    template_name = "registration/password_reset_form.html"
 
 
 # TODO BUG does it work as intended again?
-class PasswordResetSuccessView(View):
+class PasswordResetSuccessView(D_PRDV):
     breadcrumb = _l("")
-
-    def get(self, request):
-        # TODO unfortunately the extra_context is omitted and not provided
-        # TODO "The password_reset_done() view is superseded by the class-based PasswordResetDoneView()"
-        return password_reset_done(request, 'registration/password_reset_done.html')
-
-    def post(self, request):
-        return password_reset_done(request, 'registration/password_reset_done.html')
+    template_name = "registration/password_reset_done.html"
