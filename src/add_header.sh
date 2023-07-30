89dbcbed19a3c75ab56ff869b2ae9e0e5ab2345e
cauca <christian.strate@fau.de>
2023-07-30 17:07:25 +0200

    IGU-490 updated license text script for the BSD-2-Clause license

--- src/add_header.sh
+++ src/add_header.sh
@@ -3,14 +3,16 @@
 
 # this script copies the following header into all source files if it doesn't exist there yet
 : '
-Iguana (c) by Marc Ammon, Moritz Fickenscher, Lukas Fridolin,
-Michael Gunselmann, Katrin Raab, Christian Strate
+Iguana (c) by Marc Ammon, Moritz Fickenscher, Lukas Fridolin, Michael Gunselmann, Katrin Raab, Christian Strate
 
-Iguana is licensed under a
-Creative Commons Attribution-ShareAlike 4.0 International License.
+Iguana is licensed under BSD-2-Clause License.
 
-You should have received a copy of the license along with this
-work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '
 
 # This text is inserted into all html, scss, py files (__init__.py) excluded and into the logo.svg. If you want to overwrite the previous license-text you can use the "1" or "y" flag to do so. There is no need to insert this text into any css-files, since the only bigger one is the style.scss file from which style.css is generated.
@@ -31,7 +33,8 @@ LICENSE_END_PY='"""'
 LICENSE_END_SH="'"
 LICENSE_END_SCSS="\*\/"
 
-LICENSE_TEXT="Iguana (c) by Marc Ammon, Moritz Fickenscher, Lukas Fridolin,\nMichael Gunselmann, Katrin Raab, Christian Strate\n\nIguana is licensed under a\nCreative Commons Attribution-ShareAlike 4.0 International License.\n\nYou should have received a copy of the license along with this\nwork. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>."
+LICENSE_TEXT="Iguana (c) by Marc Ammon, Moritz Fickenscher, Lukas Fridolin, Michael Gunselmann, Katrin Raab, Christian Strate\n\nIguana is licensed under BSD-2-Clause License.\n\nRedistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n\n\t1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n\t2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n\nTHIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+
 TEMP="\r${LICENSE_TEXT//\\n/\\r}\r"
 # the same text as LICENSE_TEXT but with \r instead of \n because vim needs \r in write mode for a newline and slashes are escaped with prepending backslashes
 LICENSE_TEXT_REPLACE="${TEMP//\//\\/}"
